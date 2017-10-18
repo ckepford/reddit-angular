c7a054b0a652c29fd2317a09d2bbaed7cddac44d
Carey Kepford <ckepford@gmail.com>
2017-10-17 20:01:28 -0700

    added search by pipe file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+// PIPES
+import { SearchByPipe } from './pipes/search-by.pipe';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchByPipe
   ],
   imports: [
     BrowserModule,
